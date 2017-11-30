b98e5a3a13bccd63e95bdf2a59d1f458ae488dea
Paul Redmond <paulrredmond@gmail.com>
2017-11-29 20:39:30 -0700

    Update contract docs

--- src/Contracts/BladeExtension.php
+++ src/Contracts/BladeExtension.php
@@ -5,13 +5,27 @@ namespace BitPress\BladeExtension\Contracts;
 interface BladeExtension
 {
     /**
-     * Get custom directives this extension defines
+     * Get custom directives
+     *
+     * ```php
+     * return [
+     *     'truncate' => [$this, 'truncate']
+     * ];
+     * ```
+     *
      * @return array
      */
     public function getDirectives();
 
     /**
      * Get custom conditional directives
+     *
+     * ```php
+     * return [
+     *     'prod' => [$this, 'isProd']
+     * ];
+     * ```
+     *
      * @return array
      */
     public function getConditionals();
